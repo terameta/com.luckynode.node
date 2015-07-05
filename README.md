98c11cb4f739554336c0e8458e4e85808cfd6854
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-07-05 08:05:32 +0000

    Addin all files

---
+++ README.md
@@ -0,0 +1,18 @@
+# luckynode
+
+## Install
+
+You can install this package with `npm`
+
+### npm
+
+```Copy the files to a folder and run
+npm install
+nodemon
+```
+
+## License
+
+Closed License
+
+Copyright (c) LuckyNode. http://luckynode.com
---
+++ server/app.js
@@ -0,0 +1,17 @@
+{
+  "name": "node.luckynode.com",
+  "version": "0.0.1",
+  "description": "people's cloud",
+  "main": "server/app.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://alirizadikici@bitbucket.org/alirizadikici/com.luckynode.node.git"
+  },
+  "author": "LuckyNode",
+  "license": "Private",
+  "dependencies": {
+    "bcrypt-nodejs": "0.0.3"
+  }

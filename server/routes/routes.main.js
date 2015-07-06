ae81311ab3eae62d801331105769933d07512b3c
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-07-06 12:51:21 +0100

    First Route

---
+++ server/routes/routes.main.js
@@ -0,0 +1,23 @@
+module.exports = function(app, passport) {
+	app.get('/', function(req, res) {
+		//res.send('keke');
+		res.render('index');
+	});
+
+	app.get('/signin', function(req, res){
+		res.render('signin');
+	});
+
+	app.get('/home', function(req, res){
+		res.render('home');
+	});
+
+	app.post('/signin', function(req, res){
+		console.log("====================================================================");
+		console.log(req.body);
+		console.log(req.body.user);
+		console.log(req.body.pass);
+		console.log("====================================================================");
+		res.send('ok');
+	});
+};

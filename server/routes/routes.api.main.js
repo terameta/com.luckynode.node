c8a81d2153ac903bfffd134d5c54c150190be5cd
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-07-22 20:43:37 +0100

    Regular Commit

--- server/routes/routes.api.main.js
+++ server/routes/routes.api.main.js
@@ -2,29 +2,6 @@ module.exports = function(app, express, db, tools) {
 
 	var apiRoutes = express.Router();
 
-	apiRoutes.get('/setup', function(req, res) {
-		var adminUser = {
-			email: 'admin@local',
-			pass: tools.generateHash('admin@local'),
-			isAdmin: true
-		};
-		db.users.findOne({ isAdmin: true }, {pass:0}, function(err, data) {
-			if (err) { res.status(400).json(err); }
-			else {
-				if (data == null) {
-					db.users.update({ isAdmin: true }, adminUser, { upsert: true }, function(err, data) {
-						if (err) { res.status(400).json(err); }
-						else {
-							res.json(data);
-						}
-					});
-				}
-				else { res.json(data); }
-			}
-		});
-
-	});
-
 	apiRoutes.post('/authenticate', function(req, res) {
 		//console.log(req.body);
 		db.users.findOne({email:req.body.email},function(err, data){
--- server/routes/routes.main.js
+++ server/routes/routes.main.js
@@ -1,23 +1,5 @@
 module.exports = function(app, passport) {
 	app.get('/', function(req, res) {
-		//res.send('keke');
-		res.send('ok');
-	});
-
-	app.get('/signin', function(req, res){
-		res.render('signin');
-	});
-
-	app.get('/home', function(req, res){
-		res.render('home');
-	});
-
-	app.post('/signin', function(req, res){
-		console.log("====================================================================");
-		console.log(req.body);
-		console.log(req.body.user);
-		console.log(req.body.pass);
-		console.log("====================================================================");
 		res.send('ok');
 	});
 };

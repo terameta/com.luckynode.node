008586066b5a8b5453e70374995bcfa899246b3b
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-07-24 11:37:51 +0100

    Regular Commit

--- server/routes/routes.api.main.js
+++ server/routes/routes.api.main.js
@@ -3,26 +3,7 @@ module.exports = function(app, express, db, tools) {
 	var apiRoutes = express.Router();
 
 	apiRoutes.get('/authenticate', function(req, res) {
-		db.users.findOne({email:req.body.email},function(err, data){
-			if(err) { res.status(400).json(err); }
-			else {
-				if( data == null){ res.status(401).json({status:'fail'}); }
-				else {
-					if(!tools.compareHash(req.body.pass, data.pass)){
-						res.status(401).json({status:'fail'});
-					} else {
-						var token = tools.jwt.sign(data.email, app.get('jwtsecret'), {
-							expiresInMinutes: 60*24*30 // expires in 30 days
-						});
-						res.json({
-							status: 'success',
-							message: 'Enjoy your token!',
-							token: token
-						});
-					}
-				}
-			}
-		});
+		res.send(app.get('managers'));
 	});
 
 	apiRoutes.get('/', tools.checkToken, function(req, res) {

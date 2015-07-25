874c71ae093ad8319ba27e6b32b05b17d4ba4516
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-07-25 10:53:03 +0100

    Regular Commit

--- server/routes/routes.api.main.js
+++ server/routes/routes.api.main.js
@@ -29,53 +29,79 @@ module.exports = function(app, express, db, tools) {
 		
 		return deferred.promise;
 	}
+	
+	function sendToken(ip, res){
+		var token = tools.jwt.sign(ip, app.get('jwtsecret'), {
+			expiresInMinutes: 60*24*30 // expires in 30 days
+		});
+		res.json({
+			status: 'success',
+			message: 'Enjoy your token!',
+			token: token
+		});
+	}
+	
+	function simplifyIP(ip){
+		return ip.replace("::ffff:", "").replace("::FFFF:", "");
+	}
 
 	apiRoutes.get('/authenticate', function(req, res) {
 		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-		ip = ip.replace("::ffff:", "");
-		ip = ip.replace("::FFFF:", "");
+		ip = simplifyIP(ip);
 		var shouldAuth = false;
 		
 		var curManagers = app.get('managers');
 		curManagers.forEach(function(curManager){
+			curManager = curManager.replace("::ffff:", "").replace("::FFFF:", "");
 			if(curManager == ip){
 				shouldAuth = true;
+				sendToken(ip, res);
+				return 0;
 			}
 		});
 		if(!shouldAuth && curManagers.length > 0){
 			sendHTTPSRequest(curManagers[0], '/api/getManagers', false).then(
 				function(result){
 					var listofIPs = JSON.parse(result);
-					console.log("Result: ", listofIPs);
-					console.log(typeof listofIPs);
 					listofIPs.forEach(function(curManager){
-						console.log(curManager);
-						sendHTTPSRequest(curManager, '/api/getMyIPs', false).then(
-							function(mResult){ console.log(mResult); }
-						).fail(
-							function(mIssue){
-								console.log(mIssue);
-							}
-						);
+						curManager = curManager.replace("::ffff:", "").replace("::FFFF:", "");
+						if(curManager == ip){
+							shouldAuth = true;
+							sendToken(ip, res);
+							return 0;
+						}
 					});
+					if(!shouldAuth){
+						listofIPs.forEach(function(curManager){
+							curManager = curManager.replace("::ffff:", "").replace("::FFFF:", "");
+							sendHTTPSRequest(curManager, '/api/getMyIPs', false).then(
+								function(mResult){ 
+									var listofCurIPs = JSON.parse(mResult); 
+									listofCurIPs.forEach(function(curManagerIP){
+										curManagerIP = curManagerIP.replace("::ffff:", "").replace("::FFFF:", "");
+										if(curManagerIP == ip){
+											shouldAuth = true;
+											sendToken(ip, res);
+											return 0;
+										}
+									});
+								}
+							).fail(
+								function(mIssue){
+									console.log(mIssue);
+									res.status(401).json({status:'fail'});
+								}
+							);
+						});
+						res.status(401).json({status:'fail'});
+					}
 				}
 			).fail(
 				function(issue){
 					console.log("Issue: ", issue);
+					res.status(401).json({status:'fail'});
 				}
 			);
-		}
-		if(shouldAuth){
-			var token = tools.jwt.sign(ip, app.get('jwtsecret'), {
-				expiresInMinutes: 60*24*30 // expires in 30 days
-			});
-			res.json({
-				status: 'success',
-				message: 'Enjoy your token!',
-				token: token
-			});
-		} else {
-			res.status(401).json({status:'fail'});
 		}
 	});
 

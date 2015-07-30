0a072889b0e847fae3040a16a5c4f38aa02671f6
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-07-30 14:47:59 +0100

    Regular Commit

--- server/routes/routes.api.main.js
+++ server/routes/routes.api.main.js
@@ -51,6 +51,7 @@ module.exports = function(app, express, db, tools) {
 	
 	apiRoutes.get('/verifytoken', function(req, res){
 		var token = req.query.token || req.headers['x-access-token'];
+		
 		if (token) {
 			jwt.verify(token, config.secret, function(err, decoded) {
 				if (err) {

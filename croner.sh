fa0aa66df26ae26885ac3994b3e7439980d2a416
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-08-05 13:23:11 +0100

    Regular Commit

--- croner.sh
+++ croner.sh
@@ -2,7 +2,10 @@
 cd ~/node.luckynode.com
 if ! git --git-dir=".git" diff --quiet
 then
+    echo thereischange
     git reset --hard origin/master
     git fetch --all
     forever restart nodeluckynode
+else
+    echo nochange
 fi

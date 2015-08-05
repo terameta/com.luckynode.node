1ae5ba7c3c2f90f5ae8470ac352056f33a1583ee
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-08-05 14:15:52 +0100

    Regular Commit

--- croner.sh
+++ croner.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 cd ~/node.luckynode.com
-
-#STATUS="Hello World!"
-#echo $STATUS
-#git fetch origin
-#STATAS=$(git log HEAD..origin/master --oneline)
-#echo $STATAS
-
-#if ! git --git-dir=".git" diff --quiet
-#then
-#    echo thereischange
-#    git reset --hard origin/master
-#    git fetch --all
-#    forever restart nodeluckynode
-#else
-#    echo nochange
-#fi
-
 date
 git fetch origin
 reslog=$(git log HEAD..origin/master --oneline)

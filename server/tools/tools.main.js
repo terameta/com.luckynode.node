babe916811ea27b97c151580bb3b43e1060ce65c
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-08-06 06:52:55 +0100

    Regular Commit

--- server/tools/tools.main.js
+++ server/tools/tools.main.js
@@ -4,7 +4,7 @@ var config 			= require('../config/config.main.js');
 
 module.exports = {
 	checkToken : function (req, res, next) {
-		var token = req.body.token || req.query.token || req.headers['x-access-token'];
+		var token = req.headers['x-access-token'];
 		if (token) {
 			jwt.verify(token, config.secret, function(err, decoded) {
 				if (err) {

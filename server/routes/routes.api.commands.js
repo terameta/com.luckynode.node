2e1d76672f9a368313a69099ee6a2561e56789dc
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-08-06 11:26:00 +0100

    Regular Commit

--- server/routes/routes.api.commands.js
+++ server/routes/routes.api.commands.js
@@ -8,9 +8,6 @@ module.exports = function(app, express, db, tools) {
 	var apiRoutes = express.Router();
 	
 	apiRoutes.post('/assignstoragepools', tools.checkToken, function(req, res) {
-		console.log("Body: ", req.body);
-		console.log("JSON: ", req.json);
-		console.log("JSON: ", req.form);
 		res.json({ message: 'ok' });
 	});
 
--- server/tools/tools.main.js
+++ server/tools/tools.main.js
@@ -5,16 +5,6 @@ var config 			= require('../config/config.main.js');
 module.exports = {
 	checkToken : function (req, res, next) {
 		var token = req.headers['x-access-token'];
-		console.log("Body: ", req.body);
-		console.log("JSON: ", req.json);
-		console.log("JSON: ", req.form);
-		if(!token){
-			if(req.body){
-				if(req.body.token){
-					token = req.body.token;
-				}
-			}
-		}
 		if (token) {
 			jwt.verify(token, config.secret, function(err, decoded) {
 				if (err) {

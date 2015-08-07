0198c6e72792abceea7abd077624fa3ece59d131
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-08-07 20:24:52 +0100

    Regular Commit

--- server/routes/routes.api.commands.js
+++ server/routes/routes.api.commands.js
@@ -18,7 +18,7 @@ module.exports = function(app, express, db, tools) {
 			} else {
 				var result = stdout.trim().split("\n");
 				if(result.length>0){
-					var headers = result[0].split("\t");
+					var headers = result[0].split('  ');
 					console.log(headers);
 				}
 				console.log(result);

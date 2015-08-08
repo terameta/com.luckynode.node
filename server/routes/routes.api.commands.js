2ae331a95951a08a2350fd03758128b646026fad
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-08-08 13:25:15 +0100

    Regular Commit

--- server/routes/routes.api.commands.js
+++ server/routes/routes.api.commands.js
@@ -8,37 +8,48 @@ module.exports = function(app, express, db, tools) {
 	var apiRoutes = express.Router();
 	
 	apiRoutes.post('/assignstoragepools', tools.checkToken, function(req, res) {
-		var exec = require('child_process').exec;
-		var cmd = 'virsh pool-list';
+		var newPools = [];
+		if(req.body){ if(req.body.details){ if(req.body.details.length > 0){
+			newPools = req.body.details;
+		} } }
 		
-		exec(cmd, function(error, stdout, stderr) {
-			if (error) {
-				console.log(stderr);
-			} else {
-				console.log(req.body.details);
-				
-				var result = stdout.trim().split("\n");
-				if(result.length>0){
-					var headers = result[0].trim().split(/[\s,]+/);
+		if(newPools.length > 0){
+		
+			var exec = require('child_process').exec;
+			var cmd = 'virsh pool-list';
+			
+			exec(cmd, function(error, stdout, stderr) {
+				if (error) {
+					console.log(stderr);
+				} else {
+					console.log(req.body.details);
 					
-					console.log(headers);
+					
+					var result = stdout.trim().split("\n");
+					if(result.length>0){
+						var headers = result[0].trim().split(/[\s,]+/);
+						
+						console.log(headers);
+					}
+					var numPools = result.length - 2;
+					console.log("================================");
+					console.log("Number of already defined pools: ", numPools);
+					console.log("================================");
+					console.log("List below:");
+					console.log("================================");
+					var curPoolDef = [];
+					for(var curPool = 2; curPool < result.length; curPool++){
+						curPoolDef = result[curPool].trim().split(/[\s,]+/);
+						console.log(result[curPool], ">>>>>>>>>>>>>>>>>>>>>>>>",curPoolDef);
+					}
+					console.log("================================");
+					console.log("End of list");
 				}
-				var numPools = result.length - 2;
-				console.log("================================");
-				console.log("Number of already defined pools: ", numPools);
-				console.log("================================");
-				console.log("List below:");
-				console.log("================================");
-				var curPoolDef = [];
-				for(var curPool = 2; curPool < result.length; curPool++){
-					curPoolDef = result[curPool].trim().split(/[\s,]+/);
-					console.log(result[curPool], ">>>>>>>>>>>>>>>>>>>>>>>>",curPoolDef);
-				}
-				console.log("================================");
-				console.log("End of list");
-			}
-		});
-		res.json({ message: 'ok' });
+			});
+			res.send("ok");
+		} else {
+			res.status(400).json({ status: 'fail', detail: 'no data provided' });
+		}
 	});
 
 	app.use('/api/command', apiRoutes);

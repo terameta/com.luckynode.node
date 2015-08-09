f95239ee5520135c7d56d6d8bee39c88f087ee90
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-08-09 19:46:38 +0100

    Regular Commit

--- server/routes/routes.api.commands.js
+++ server/routes/routes.api.commands.js
@@ -55,7 +55,7 @@ module.exports = function(app, express, db, tools) {
 					console.log(poolsToRemove);
 					console.log("===============");console.log("===============");console.log("===============");console.log("===============");
 					
-					/*virsh.poolsDefine(poolsToDefine).then(
+					virsh.poolsDefine(poolsToDefine).then(
 						function(result){
 							console.log("Pools Define Succeded:", result);
 						}
@@ -63,7 +63,7 @@ module.exports = function(app, express, db, tools) {
 						function(issue){
 							console.log("Pools Define Failed:", issue);
 						}
-					);*/
+					);
 					virsh.poolsRemove(poolsToRemove).then(
 						function(result){
 							console.log("Pools Remove Succeeded:", result);
@@ -76,124 +76,6 @@ module.exports = function(app, express, db, tools) {
 				}
 			);
 			
-			
-			
-			return 0;
-			
-			exec(cmd, function(error, stdout, stderr) {
-				if (error) {
-					console.log(stderr);
-				} else {
-					console.log(stdout);
-					
-					
-					var result = stdout.trim().split("\n");
-					var numPools = result.length - 2;
-					if(numPools < 0) numPools = 0;
-					
-					
-					console.log("===# new Pools: ", newPools.length);
-					console.log("===# exs Pools: ", numPools);
-					console.log("================================");
-					console.log("===We will now identify non-existing pools");
-					for(var curNewPool = 0; curNewPool < newPools.length; curNewPool++){
-						var virshCommand = '';
-						console.log("======Working on:", newPools[curNewPool].name);
-						var curPoolDef = [];
-						var shouldDefine = true;
-						for(var curPool = 2; curPool < result.length; curPool++){
-							curPoolDef = result[curPool].trim().split(/[\s,]+/);
-							console.log(result[curPool].trim(), ">>>>>>>>>>>>>>>>>>>>>>>>",curPoolDef);
-							if(curPoolDef[0] == newPools[curNewPool].name) shouldDefine = false;
-						}
-						if(shouldDefine){
-							console.log("======This pool is not yet defined. We will now define");
-							console.log("virsh pool-define-as store0 netfs --source-host=store0.luckynode.com --source-path=/var/store0 --target=/var/store0");
-							virshCommand = "virsh pool-define-as " + newPools[curNewPool].name + " netfs";
-							virshCommand += " --source-host=" + newPools[curNewPool].source.split(":")[0];
-							virshCommand += " --source-path=" + newPools[curNewPool].source.split(":")[1];
-							virshCommand += " --target=/mnt/luckynodepools/"+newPools[curNewPool].name;
-							
-							tools.runLocalCommand(virshCommand).then(
-								function(result){ console.log(result); }
-							).fail(
-								function(issue){ console.log(issue); }
-							);
-							console.log(virshCommand);
-							console.log(newPools[curNewPool]);
-						}
-						
-						virshCommand = 'virsh pool-build ' + newPools[curNewPool].name;
-						tools.runLocalCommand(virshCommand).then(
-							function(result){ console.log(result.trim()); }
-						).fail(
-							function(issue){ console.log(issue.trim()); }
-						);
-						
-						virshCommand = 'virsh pool-autostart ' + newPools[curNewPool].name;
-						tools.runLocalCommand(virshCommand).then(
-							function(result){ console.log(result.trim()); }
-						).fail(
-							function(issue){ console.log(issue.trim()); }
-						);
-						virshCommand = 'virsh pool-start ' + newPools[curNewPool].name;
-						tools.runLocalCommand(virshCommand).then(
-							function(result){ console.log(result.trim()); }
-						).fail(
-							function(issue){ console.log(issue.trim()); }
-						);
-					}
-					
-					console.log("===========================================================");
-					console.log("Removing the non-assigned ones");
-					console.log("===========================================================");
-					for(var curPool = 2; curPool < result.length; curPool++){
-						curPoolDef = result[curPool].trim().split(/[\s,]+/);
-						var exsPoolName = curPoolDef[0] || 'NoName|||||||||||||||||||||||||||||';
-						var exsPoolIsActive = curPoolDef[1] == 'active' ? true : false;
-						var shouldRemove = true;
-						for(var curNewPool = 0; curNewPool < newPools.length; curNewPool++){
-							console.log(newPools[curNewPool]);
-							console.log("Existing Pool:", exsPoolName);
-							console.log("New Pool:",newPools[curNewPool].name);
-							if(exsPoolName == newPools[curNewPool].name){
-								shouldRemove = false;
-							}
-							console.log("Should Remove:", shouldRemove);
-							console.log("Should Remove:", exsPoolName);
-						}
-						if(shouldRemove){
-							console.log("<<<<<<<<<<<<<<<<<<<<<<<<||||||||||||||||||||||||||||||>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-							console.log("<<<<<<<<<<<<<<<<<<<<<<<<We are running removal for " + exsPoolName + ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-							virshCommand = "virsh pool-destroy " + exsPoolName;
-							tools.runIfLocalCommand(virshCommand, exsPoolName, exsPoolIsActive).then(
-								function(curPoolName){
-									console.log(curPoolName);
-									virshCommand = "virsh pool-delete " + curPoolName;
-									return tools.runLocalCommand(virshCommand, curPoolName);
-								}
-							).then(
-								function(curPoolName){
-									console.log(curPoolName);
-									virshCommand = "virsh pool-undefine " + curPoolName;
-									return tools.runLocalCommand(virshCommand);
-								}
-							).then(
-								function(result){
-									console.log(result);
-								}
-							).fail(
-								function(issue){
-									console.log("<<<<<<<<<<<<<<<<<<<<<<<<||||||||||||||||||||||||||||||>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-									console.log("<<<<<<<<<<<<<<<<<<<<<<<<There is an issue with command>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-									console.log(issue);
-									console.log(virshCommand);
-								}
-							);
-						}
-					}
-				}
-			});
 			res.send("ok");
 		} else {
 			res.status(400).json({ status: 'fail', detail: 'no data provided' });

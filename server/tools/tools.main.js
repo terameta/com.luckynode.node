496466fa5ed29336955eec4a3dc80fae9972873f
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-08-09 13:08:38 +0100

    Regular Commit

--- server/tools/tools.main.js
+++ server/tools/tools.main.js
@@ -71,7 +71,7 @@ function runLocalCommands(commandList){
 		var curCommand = commandList.shift();
 		runLocalCommand(curCommand).then(
 			function(){
-				deferred.resolve(runLocalCommands(commandList));
+				deferred.resolve(function(){return runLocalCommands(commandList);});
 			}
 		).fail(deferred.reject);
 	} else {

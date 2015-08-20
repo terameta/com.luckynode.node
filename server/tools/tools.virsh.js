d6402b119ed011a9534fd15321dc1c5fcaea9fed
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-08-20 19:52:47 +0100

    Regular Commit

--- server/tools/tools.virsh.js
+++ server/tools/tools.virsh.js
@@ -19,54 +19,14 @@ function serverDefine(cSrv){
 	if(!cSrv.architecture) 	cSrv.architecture = 'x86_64';
 	if(!cSrv.imageType) 	cSrv.imageType = 'qcow2';
 	
-	getMostAvailablePool();
-	
-	var theXML = ''
-	+ 	'<domain type=\'kvm\'>'																					+ '\n'
-	+ 	'	<name>'+ cSrv.id +'</name>'																			+ '\n'
-	//  <uuid>e5c82363-ceea-74a2-a678-c05ab504c669</uuid>														+ '\n'
-	+ 	'	<description>'+ cSrv.name +'</description>'															+ '\n'
-	+ 	'	<memory unit=\'MiB\'>'+ cSrv.ram +'</memory>'														+ '\n'
-	+ 	'	<vcpu placement=\'static\'>'+ cSrv.cpu +'</vcpu>'													+ '\n'
-	+ 	'	<cpu><topology sockets=\'1\' cores=\''+ cSrv.cpu +'\' threads=\'1\'/></cpu>'						+ '\n'
-	+ 	'	<os>'																								+ '\n'
-	+ 	'		<type arch=\''+ cSrv.architecture +'\' machine=\'pc\'>hvm</type>'								+ '\n'
-	+ 	'		<boot dev=\'hd\' />'																			+ '\n'
-	+ 	'		<boot dev=\'cdrom\' />'																			+ '\n'
-	+ 	'		<bootmenu enable=\'yes\' timeout=\'3000\' />'													+ '\n'
-	+ 	'	</os>'																								+ '\n'
-	+	'	<features><acpi /><apic /><pae /></features>'														+ '\n'
-	+	'	<clock sync=\'localtime\'/>'																		+ '\n'
-	+	'	<on_poweroff>destroy</on_poweroff>'																	+ '\n'
-	+	'	<on_reboot>restart</on_reboot>'																		+ '\n'
-	+	'	<on_crash>restart</on_crash>'																		+ '\n'
-	+	'	<devices>'																							+ '\n'
-	+	'		<disk type=\'file\' device=\'disk\'>'															+ '\n'
-	+	'			<driver name=\'qemu\' type=\''+ cSrv.imageType +'\' cache=\'none\' />'						+ '\n'
-	+	'			<source file=\'/mnt/luckynodepools/store0/deneme.qcow2\' />'								+ '\n'
-	+	'			<target dev=\'vda\' bus=\'virtio\'/>'														+ '\n'
-	// if ide use this +	'			<target dev=\'hda\' bus=\'ide\'/>'										+ '\n'
-	+	'		</disk>'																						+ '\n'
-	+	'		<disk type=\'file\' device=\'cdrom\'><target dev=\'hdc\'/><readonly/></disk>'					+ '\n'
-	+	'		<interface type=\'bridge\'>'																	+ '\n'
-	// if other driver we should write e1000 or stg
-	+	'			<model type=\'virtio\' />'																	+ '\n'
-	+	'			<source bridge=\'br0\'/>'																	+ '\n'
-	//for below target dev we should find a proper naming mechanism
-//	+	'			<target dev=\'kvm255.0\'/>'																	+ '\n'
-	+	'			<mac address=\''+ cSrv.mac +'\'/>'															+ '\n'
-	+	'		</interface>'																					+ '\n'
-	+	'		<input type=\'tablet\'/>'																		+ '\n'
-	+	'		<input type=\'mouse\'/>'																		+ '\n'
-	+	'		<graphics type=\'vnc\' port=\'-1\' autoport=\'yes\' passwd=\'Xw2oACxc\' listen=\'0.0.0.0\'/>'	+ '\n'
-	//We should eventually control the vnc ports
-// 	+	'		<graphics type=\'vnc\' port=\'5901\' autoport=\'yes\' passwd=\'Xw2oACxc\' listen=\'0.0.0.0\'/>'	+ '\n'
-	+	'		<video><model type=\'vga\' vram=\'9216\' heads=\'1\'/></video>'									+ '\n'
-	+	'	</devices>'																							+ '\n'
-	+ 	'</domain>';
-	console.log(theXML);
+	getMostAvailablePool(cSrv).
+		then(composeDomainXML).
+		fail(function(issue){
+			deferred.reject(issue);
+		});
+
 	var fs = require('fs');
-	fs.writeFile('/tmp/'+cSrv.id+'.xml', theXML, function(err) {
+	fs.writeFile('/tmp/'+cSrv.id+'.xml', csrv.theXML, function(err) {
 		if (err){
 			console.log(err);
 		} else {
@@ -179,8 +139,12 @@ function poolList(){
 	return deferred.promise;
 }
 
-function getMostAvailablePool(){
+function getMostAvailablePool(cSrv){
 	var deferred = Q.defer();
+	if(cSrv.store){
+		deferred.resolve();
+		return deferred.promise;
+	}
 	tools.runLocalCommand('virsh pool-list --details').then(function(result){
 		result = result.trim().split("\n");
 		result.splice(0,2);
@@ -228,10 +192,62 @@ function getMostAvailablePool(){
 			
 		});
 		if(curMax !=''){
-			deferred.resolve(curMax);
+			cSrv.store = curMax;
+			deferred.resolve(cSrv);
 		} else {
 			deferred.reject('There are no stores available');
 		}
 	}).fail(function(issue){deferred.reject(issue);});
+	return deferred.promise;
+}
+
+function composeDomainXML(cSrv){
+	var deferred = Q.defer();
+	var theXML = ''
+	+ 	'<domain type=\'kvm\'>'																					+ '\n'
+	+ 	'	<name>'+ cSrv.id +'</name>'																			+ '\n'
+	//  <uuid>e5c82363-ceea-74a2-a678-c05ab504c669</uuid>														+ '\n'
+	+ 	'	<description>'+ cSrv.name +'</description>'															+ '\n'
+	+ 	'	<memory unit=\'MiB\'>'+ cSrv.ram +'</memory>'														+ '\n'
+	+ 	'	<vcpu placement=\'static\'>'+ cSrv.cpu +'</vcpu>'													+ '\n'
+	+ 	'	<cpu><topology sockets=\'1\' cores=\''+ cSrv.cpu +'\' threads=\'1\'/></cpu>'						+ '\n'
+	+ 	'	<os>'																								+ '\n'
+	+ 	'		<type arch=\''+ cSrv.architecture +'\' machine=\'pc\'>hvm</type>'								+ '\n'
+	+ 	'		<boot dev=\'hd\' />'																			+ '\n'
+	+ 	'		<boot dev=\'cdrom\' />'																			+ '\n'
+	+ 	'		<bootmenu enable=\'yes\' timeout=\'3000\' />'													+ '\n'
+	+ 	'	</os>'																								+ '\n'
+	+	'	<features><acpi /><apic /><pae /></features>'														+ '\n'
+	+	'	<clock sync=\'localtime\'/>'																		+ '\n'
+	+	'	<on_poweroff>destroy</on_poweroff>'																	+ '\n'
+	+	'	<on_reboot>restart</on_reboot>'																		+ '\n'
+	+	'	<on_crash>restart</on_crash>'																		+ '\n'
+	+	'	<devices>'																							+ '\n'
+	+	'		<disk type=\'file\' device=\'disk\'>'															+ '\n'
+	+	'			<driver name=\'qemu\' type=\''+ cSrv.imageType +'\' cache=\'none\' />'						+ '\n'
+	+	'			<source file=\'/mnt/luckynodepools/store0/deneme.qcow2\' />'								+ '\n'
+	+	'			<target dev=\'vda\' bus=\'virtio\'/>'														+ '\n'
+	// if ide use this +	'			<target dev=\'hda\' bus=\'ide\'/>'										+ '\n'
+	+	'		</disk>'																						+ '\n'
+	+	'		<disk type=\'file\' device=\'cdrom\'><target dev=\'hdc\'/><readonly/></disk>'					+ '\n'
+	+	'		<interface type=\'bridge\'>'																	+ '\n'
+	// if other driver we should write e1000 or stg
+	+	'			<model type=\'virtio\' />'																	+ '\n'
+	+	'			<source bridge=\'br0\'/>'																	+ '\n'
+	//for below target dev we should find a proper naming mechanism
+//	+	'			<target dev=\'kvm255.0\'/>'																	+ '\n'
+	+	'			<mac address=\''+ cSrv.mac +'\'/>'															+ '\n'
+	+	'		</interface>'																					+ '\n'
+	+	'		<input type=\'tablet\'/>'																		+ '\n'
+	+	'		<input type=\'mouse\'/>'																		+ '\n'
+	+	'		<graphics type=\'vnc\' port=\'-1\' autoport=\'yes\' passwd=\'Xw2oACxc\' listen=\'0.0.0.0\'/>'	+ '\n'
+	//We should eventually control the vnc ports
+// 	+	'		<graphics type=\'vnc\' port=\'5901\' autoport=\'yes\' passwd=\'Xw2oACxc\' listen=\'0.0.0.0\'/>'	+ '\n'
+	+	'		<video><model type=\'vga\' vram=\'9216\' heads=\'1\'/></video>'									+ '\n'
+	+	'	</devices>'																							+ '\n'
+	+ 	'</domain>';
+	cSrv.theXML = theXML;
+	console.log(theXML);
+	deferred.resolve(cSrv);
 	return deferred.promise;
 }

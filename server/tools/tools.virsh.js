5127fdc7009c26dc4136963029eb5cf889d94cc2
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-10-18 08:19:10 +0100

    Regular Commit

--- server/tools/tools.virsh.js
+++ server/tools/tools.virsh.js
@@ -145,6 +145,7 @@ function getNBDPID(cSrv){
 		for(var i = 0; i < result.length; i++){
 			if(result[i].indexOf(cSrv.id+'.qcow2') >= 0){
 				console.log("This is our guy", result[i]);
+				console.log(tools.splitBySpace(result[i]));
 			} else {
 				console.log("This is not our guy", result[i]);
 			}

e065f009f785133d36775c8a586dcda1482337af
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-10-29 06:23:31 +0000

    Regular Commit

--- trimnetw.sh
+++ trimnetw.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ~/node.luckynode.com
-cat /etc/network/interfaces | awk '{$1=$1};1' > interfaces 
+cat /etc/network/interfaces | awk '{$1=$1};1' > interfaces
+mv interfaces /etc/network/interfaces

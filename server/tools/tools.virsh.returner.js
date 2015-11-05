48da491823024fb0f6a5ab3af48024ebe3ff80f8
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-11-05 10:22:24 +0000

    Regular Commit

--- server/tools/tools.virsh.returner.js
+++ server/tools/tools.virsh.returner.js
@@ -8,6 +8,7 @@ function prepare(result, command){
 	var deferred = Q.defer();
 	var wte = ''; 		//What to Expect
 	if(command == 'vol-list'){	wte = ['Name', 'Path', 'Type', 'Capacity', 'Allocation'];	}
+	console.log(result);
 	console.log(wte);
 	deferred.resolve(result);
 	return deferred.promise;

f1f19fae47b415b08593aab6b02b0e4bbbb3b37b
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-11-11 13:50:44 +0000

    Regular Commit

--- server/tools/tools.crons.js
+++ server/tools/tools.crons.js
@@ -1,17 +1,24 @@
 var topDB = {};
 
 module.exports = function(db){
+    topDB = db;
     var curModule = {
-        kekele: getCollectionNames
+        kekele: getCollectionNames,
+        everytensecs: everytensecs
     };
-    
+    /*
     curModule.everytensecs = function(){
         console.log(new Date(), topDB, db);
     };
+    */
     
     return curModule;
 };
 
 function getCollectionNames(){
 	console.log("This is kekele");
+}
+
+function everytensecs(){
+    console.log(new Date(), topDB, db);
 }

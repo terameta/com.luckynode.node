bac7cf0ac8bc0894615f55076bc294076aee3672
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-11-11 13:19:51 +0000

    Regular Commit

---
+++ server/tools/tools.crons.js
@@ -0,0 +1,21 @@
+var topDB = {};
+
+module.exports = function crons(db) {
+	topDB = db;
+
+	var cronFunctions = {
+		getCollectionNames: getCollectionNames
+	};
+
+	return cronFunctions;
+};
+
+function getCollectionNames(){
+	topDB.getCollectionNames(function(err, result){
+		if(err){
+			console.log("Get Collection Names Error", err);
+		} else {
+			console.log("List of Collections:", result);
+		}
+	});
+}

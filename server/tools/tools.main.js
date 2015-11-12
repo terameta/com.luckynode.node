996ad7a7eca716862f21425c1505f8ad8ba7b3bc
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-11-12 06:15:54 +0000

    Regular Commit

--- server/tools/tools.main.js
+++ server/tools/tools.main.js
@@ -7,6 +7,11 @@ var fs 				= require("fs");
 var mongojs 		= require('mongojs');
 
 var whoami			= JSON.parse(fs.readFileSync('whoami.conf', 'utf8')).whoami;
+console.log("===============================================================");
+console.log("===============================================================");
+console.log(whoami);
+console.log("===============================================================");
+console.log("===============================================================");
 
 var dbconfig;
 

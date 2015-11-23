7982743fcc38b46fbd8454a1e9ce01a9209e21cb
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-11-23 18:13:00 +0000

    Regular Commit

--- server/tools/tools.virsh.server.js
+++ server/tools/tools.virsh.server.js
@@ -299,7 +299,7 @@ function writeDHCPItem(cSrv){
 }
 
 function deleteDHCPItem(cSrv){
-	tools.logger.info("writeServerDHCPItem is called", cSrv.id, true);
+	tools.logger.info("deleteDHCPItem is called", cSrv.id, true);
 	var deferred = Q.defer();
 	var theCommands = [];
 	theCommands.push('cd && rm dhcpd.conf.body.'+cSrv.id);

a81bd7ad087cfc659d093a3a09311eb0eab8bde9
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-12-28 08:21:54 +0000

    Regular Commit

--- server/tools/tools.main.js
+++ server/tools/tools.main.js
@@ -36,7 +36,7 @@ catch (err) {
 }
 
 var cloudConnStr	= dbconfig.user+':'+dbconfig.pass+'@'+dbconfig.server+':'+dbconfig.port+'/'+dbconfig.database;
-var cloudColls		= ['logs', 'servers', 'nodes'];
+var cloudColls		= ['logs', 'servers', 'nodes','storages'];
 var db 				= mongojs(cloudConnStr, cloudColls, { authMechanism : 'ScramSHA1' });
 
 var logger = {

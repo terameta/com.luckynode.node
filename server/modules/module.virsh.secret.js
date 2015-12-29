ad762028b30f361de6484cc1a9a58ccfe3d98c38
Ali Riza Dikici <aliriza.dikici@gmail.com>
2015-12-29 07:06:23 +0000

    Regular Commit

--- server/modules/module.virsh.secret.js
+++ server/modules/module.virsh.secret.js
@@ -6,7 +6,6 @@ var tools			= require('../tools/tools.main.js');
 var returner		= require('../tools/tools.virsh.returner.js');
 
 module.exports = function(){
-	console.log("Secret Module is getting defined now");
 	var module = {
 		list: list
 	};
@@ -14,7 +13,9 @@ module.exports = function(){
 };
 
 function list(toReturn){
-	console.log("SecretList Called inside<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+	console.log("ToReturn<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+	console.log("ToReturn:\n",toReturn);
+	console.log("ToReturn<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 	if(!toReturn) toReturn = {};
 	var deferred = Q.defer();
 	tools.runLocalCommand("virsh secret-list").
@@ -22,10 +23,13 @@ function list(toReturn){
 		return returner.prepare(result,'secret-list');
 	}).
 	then(function(result){
-		console.log("SecretList<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-		console.log("Returner Result:\n",result);
-		console.log("SecretList<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		
 		toReturn.secretList = result;
+		
+		console.log("ToReturn<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		console.log("ToReturn After:\n",toReturn);
+		console.log("ToReturn<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		
 		deferred.resolve(toReturn);
 	}).fail(deferred.reject);
 	return deferred.promise;

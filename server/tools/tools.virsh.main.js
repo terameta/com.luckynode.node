ae296e08d80f55e0396db8f6d6b9c7361b362470
Ali Riza Dikici <aliriza.dikici@gmail.com>
2016-01-04 12:13:09 +0000

    Regular Commit

--- server/tools/tools.virsh.main.js
+++ server/tools/tools.virsh.main.js
@@ -112,9 +112,10 @@ function refreshDHCPConfig(){
 		});
 		interfaceString = interfaceList.join(' ');
 		theCommands.push('sudo sh -c \'echo INTERFACE=\\\"'+ interfaceString +'\\\" > /etc/default/isc-dhcp-server\'');
-		theCommands.push('cd && echo "subnet 0.0.0.0 netmask 0.0.0.0 {authoritative;default-lease-time 21600000;max-lease-time 432000000;}\\nddns-update-style none;\\n" > dhcpd.conf.head');
-		theCommands.push('cd && echo "\\n" > dhcpd.conf.body.0');
-		theCommands.push('cd && echo "\\n" > dhcpd.conf.body.1');
+		theCommands.push('cd && echo "subnet 0.0.0.0 netmask 0.0.0.0 {authoritative;default-lease-time 21600000;max-lease-time 432000000;}" > dhcpd.conf.head');
+		theCommands.push('cd && echo "ddns-update-style none;" >> dhcpd.conf.head');
+		theCommands.push('cd && echo " " > dhcpd.conf.body.0');
+		theCommands.push('cd && echo " " > dhcpd.conf.body.1');
 		theCommands.push('cd && sudo sh -c "cat dhcpd.conf.head dhcpd.conf.body* > /etc/dhcp/dhcpd.conf"');
 		theCommands.push('sudo service isc-dhcp-server restart');
 		tools.runLocalCommands(theCommands).then(function(result) {

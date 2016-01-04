00f54ece9739edf5f41869aaf6f82e7dc3dc04f5
Ali Riza Dikici <aliriza.dikici@gmail.com>
2016-01-04 07:12:35 +0000

    Regular Commit

--- server/tools/tools.virsh.volume.js
+++ server/tools/tools.virsh.volume.js
@@ -48,6 +48,8 @@ function create(diskName, pool, size, type, bVol){
 
 function resize(name, pool, capacity){
 	var deferred = Q.defer();
+	var commandList = [];
+	commandList.push("virsh pool-refresh --pool " + pool);
 	var curCommand = "sudo virsh vol-resize --vol "+ name +" --pool "+ pool +" --capacity "+ capacity +"G";
 	tools.runLocalCommand(curCommand).then(function(result) {
 		deferred.resolve(result);

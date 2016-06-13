60f4286ef90b3f78dce90824684dafcf5bd9344b
Ali Riza Dikici <aliriza.dikici@gmail.com>
2016-06-13 07:53:35 +0100

    Regular Commit

--- server/tools/tools.virsh.secret.js
+++ server/tools/tools.virsh.secret.js
@@ -3,10 +3,18 @@ var tools			= require('../tools/tools.main.js');
 var returner		= require('../tools/tools.virsh.returner.js');
 
 module.exports = {
-	list:		list
+	list:		list,
+	define:	define
 };
 
 function list(){
-	tools.logger.info("serverList is called");
+	tools.logger.info("Secret List is called");
 	return  tools.runLocalCommand('virsh secret-list').then(function(result){ return returner.prepare(result, 'secret-list') });
+}
+
+function define(){
+	tools.logger.info("Secret Define is called");
+	var deferred = Q.defer();
+	deferred.resolve("We are now defining");
+	return deferred.promise;
 }

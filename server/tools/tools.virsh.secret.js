ad3936a689d4e3ac75966d4adfcee3ffb2009d67
Ali Riza Dikici <aliriza.dikici@gmail.com>
2016-06-13 08:50:04 +0100

    Regular Commit

--- server/tools/tools.virsh.secret.js
+++ server/tools/tools.virsh.secret.js
@@ -12,11 +12,42 @@ function list(){
 	return  tools.runLocalCommand('virsh secret-list').then(function(result){ return returner.prepare(result, 'secret-list') });
 }
 
-function define(secretInfo){
+function define(refObject){
 	tools.logger.info("Secret Define is called");
-	console.log(secretInfo);
+	console.log(refObject);
 	var deferred = Q.defer();
 	deferred.resolve("We are now defining");
-	list().then(console.log);
+	exists(refObject);
+	return deferred.promise;
+}
+
+function exists(curPool){
+	var deferred = Q.defer();
+	list().then(function(secretList){
+		secretList.forEach(function(curSecret){
+			console.log("Exists:", curSecret);
+		});
+	}).fail(deferred.reject);
+	return deferred.promise;
+}
+
+function saveSecretXML(curPool){
+	var deferred = Q.defer();
+	var fs = require('fs');
+	console.log("=============================================================");
+	console.log("=============================================================");
+	console.log(curPool);
+	console.log("=============================================================");
+	console.log("=============================================================");
+	curPool.secretXML = "<secret ephemeral='no' private='yes'><uuid>"+curPool.secretuuid+"</uuid><usage type='ceph'><name>"+curPool.rbdname+" secret</name></usage></secret>";
+	curPool.secretFile = '/tmp/'+curPool.id+'-secret.xml';
+	fs.writeFile(curPool.secretFile, curPool.secretXML, function(err) {
+		if (err){
+			deferred.reject(err);
+		} else {
+			tools.logger.info("Secret XML file for the new pool " + curPool.id + " is saved.", true);
+			deferred.resolve(curPool);
+		}
+	});
 	return deferred.promise;
 }

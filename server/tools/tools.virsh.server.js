5b89b60d951b584778eb5a248b501a0533d482ab
Ali Riza Dikici <aliriza.dikici@gmail.com>
2016-06-14 12:51:40 +0100

    Regular Commit

--- server/tools/tools.virsh.server.js
+++ server/tools/tools.virsh.server.js
@@ -31,7 +31,7 @@ function migrate(details){
 	var deferred = Q.defer();
 	console.log(details);
 	deferred.resolve();
-	fetchServerFromDB(details.server).then(console.log);
+	fetchServerFromDB({_id: details.server}).then(console.log);
 	return deferred.promise;
 }
 

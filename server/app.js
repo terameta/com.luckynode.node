b726556ad0ed78b7a3fc6b3ebd6ed138df0ab2dc
Ali Riza Dikici <aliriza.dikici@gmail.com>
2016-06-19 11:01:06 +0100

    Regular Commit

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ catch (err) {
 
 var cloudConnStr	= dbconfig.connstr;
 var cloudColls		= ['users','datacenters','nodes','ipblocks','storages','nodecs','nodetokens','managers','plans','servers','images','isofiles'];
-var db 				= mongojs(cloudConnStr, cloudColls, { authMechanism : 'ScramSHA1' });
+var db 				= mongojs(cloudConnStr, cloudColls, { authMechanism : 'ScramSHA1', connectionTimeout: 30000 });
 //console.log(db);
 //console.log(cloudConnStr);
 

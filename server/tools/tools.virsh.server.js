b89781bfdc8c00bfcad38379304876676e26b913
Ali Riza Dikici <aliriza.dikici@gmail.com>
2016-06-21 11:27:16 +0400

    Regular Commit

--- server/tools/tools.virsh.server.js
+++ server/tools/tools.virsh.server.js
@@ -179,7 +179,7 @@ function migrationUpdateServerXML(refObject){
 			var slText = refObject.serverXML.substring(slStart, slEnd); 
 			console.log(slStart, slEnd);
 			console.log("+++"+slText+"+++");
-			refObject.serverXML = refObject.serverXML.replace(slText, "<seclabel type='dynamic' model='apparmor'/>");
+			//refObject.serverXML = refObject.serverXML.replace(slText, "<seclabel type='dynamic' model='apparmor'/>");
 			console.log("=======================================================");
 			console.log("=======================================================");
 			deferred.resolve(refObject);
